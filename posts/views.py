de5c5fb41d5ad032ebd4ce76a342311ef6c29d6a
jocnn <ijann_@outlook.com>
2022-07-23 13:58:14 -0300

    se agregó permisos a nivel de proyecto

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,19 @@
-from rest_framework import generics
+from rest_framework import generics  # , permissions
+
+from .permissions import IsAuthorOrReadOnly
 
 from .models import Post
 from .serializers import PostSerializers
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializers
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializers
