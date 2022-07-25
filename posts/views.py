b814256bc5455c3ea2f5269ab0f3499da0cc8b7d
jocnn <ijann_@outlook.com>
2022-07-25 10:22:59 -0300

    se agregó permisos para los usuarios

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from .permissions import IsAuthorOrReadOnly
 
@@ -14,5 +15,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUser]
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
